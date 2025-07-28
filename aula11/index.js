cfba06866124a68b38d12087fd2303a8ab98d9dd
ASAPNYEL <danyelhenrique941@gmail.com>
2025-07-28 14:11:46 -0300

    Create index.js

---
+++ aula11/index.js
@@ -0,0 +1,10 @@
+var velocidade = 59
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+
+if(velocidade > 60){
+    console.log('Voce ultraprassou o limite de velocidade. MULTADO!')
+}else{
+    console.log('Boa Viagem! Dirija sempre usando o cinto de segurança!')
+}
+
+// console.log('Dirija sempre usando o cinto de segurança!')
