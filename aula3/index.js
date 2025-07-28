a9349baa9daac8603b2ba476b073e4bbffcee5ba
ASAPNYEL <danyelhenrique941@gmail.com>
2025-07-28 10:16:57 -0300

    Create index.js

---
+++ aula3/index.js
@@ -0,0 +1,8 @@
+var number_one = Number.parseInt(prompt("Digite um numero: "));
+
+var number_two = Number.parseInt(prompt("Digite outro numero: "));
+
+
+var total = number_one + number_two;
+
+alert(`A soma entre ${number_one} + ${number_two} é = ${total}`);
